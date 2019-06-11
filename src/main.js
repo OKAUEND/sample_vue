e7183cf14b7f6f14b5c6b2158528f246f4538887
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-06-12 03:04:16 +0900

    マージ

--- src/main.js
+++ src/main.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Vue from 'vue'
 import App from './App.vue'
 
@@ -7,15 +6,3 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
-=======
-import Vue from 'vue/dist/vue.esm.js';
-// import App from './App.vue'
-
-new Vue({
-  el:'#app',
-  data:{
-    message:'Hello World!',
-    name:'I am Vue!'
-  }
-})
->>>>>>> 739cf027f2865ec00d24021bfe7edac0a92c98db
