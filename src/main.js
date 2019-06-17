06498c64689b10250a2f842532da5fdef485a823
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-06-17 21:37:12 +0900

    チュートリアル勉強その2

--- src/main.js
+++ src/main.js
@@ -27,3 +27,50 @@ let app3 = new Vue({
     seen:true
   }
 })
+
+let app4 = new Vue({
+  el:"#app4" ,
+  data: {
+    todos:[
+      { text: 'Javascript学習中！'} ,
+      { text: 'Vue学習中！'} ,
+      { text: '素晴らしいものを作ろう！'}
+    ]
+  }
+})
+
+let app5 = new Vue({
+  el:'#app5',
+  data:{
+    message: 'Hello Vue.js !'
+  },
+  methods:{
+    reverseMessage:function()
+    {
+      this.message = this.message.split('').reverse().join('')
+    }
+  }
+})
+
+let app6 = new Vue({
+  el:'#app6',
+  data:{
+    message:'Hello Vue!'
+  }
+})
+
+Vue.component('todo-item',{
+  props:['todo'],
+  template:'<li>{{todo.text}}</li>'
+})
+
+let app7 = new Vue({
+  el:"#app7",
+  data:{
+    groceryList:[
+      {id:0,text:'Vegetables'},
+      {id:1,text:'Cheese'},
+      {id:2,text:'Whatever eles humans are supposed to eat'}
+    ]
+  }
+})
