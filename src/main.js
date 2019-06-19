6c023671fa2e76a745580afd3d3ce4e89f19f0d8
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-06-19 14:50:28 +0900

    チュートリアルその3 算出プロパティとウォッチ

--- src/main.js
+++ src/main.js
@@ -74,3 +74,105 @@ let app7 = new Vue({
     ]
   }
 })
+
+var obj =
+{
+  foo:'bozz'
+}
+
+new Vue({
+  el:'#app8',
+  data:obj
+})
+
+let app9 = new Vue({
+  el:'#app9',
+  data:{
+    number: 10,
+    ok:false,
+    message:'FFXIV',
+    id:50
+  }
+})
+
+var example = new Vue({
+  el:"#example",
+  data:{
+    message:'Hello'
+  },
+  computed:{
+    reversedMessage:function(){
+      return this.message.split('').reverse().join('')
+    }
+  }
+})
+
+console.log(example.reversedMessage);
+example.message = 'GoodBye'
+console.log(example.reversedMessage);
+example.message = 'Test'
+
+let fullname = new Vue({
+  el:'#demo',
+  data:{
+    firstName:'Foo',
+    lastName:'Bar',
+    fullName:'',
+  },
+  conputed:{
+    fullName:{
+      get:function() {
+        return this.firstName + ' ' + this.lastName
+      },
+      set:function(val)
+      {
+        let names = val.split('')
+        this.firstName = names[0]
+        this.lastName = names[names.length - 1]
+      }
+    }
+  }
+})
+
+fullname.fullName = 'Hello World'
+console.log(fullname.fullName)
+
+let watchExampleVM = new Vue({
+  el:'#watch-example',
+  data:{
+    question:'',
+    answer:'あなたが質問をするまで私はあなたに答えを与えることはできません！'
+  },
+  watch:{
+    question: function (newQuestion,oldQuestion)
+    {
+      this.answer = '入力を中止するのを待っています...'
+      this.debouncedGetAnswer()
+    }
+  },
+  created:function()
+  {
+    this.debouncedGetAnswer = _.debounce(this.getAnswer , 500)
+  },
+  methods:
+  {
+    getAnswer:function()
+    {
+      if(this.question.indexOf('?') === -1)
+      {
+        this.answer = '質問には疑問符が含まれています(´・ω・`)'
+        return
+      }
+      this.answer = '待機中…'
+      let vm = this
+      axios.get('https://yesno.wtf/api')
+        .then(function(response)
+        {
+          vm.answer = _.capitalize(response.data.answer)
+        })
+        .catch(function (error) {
+          vm.answer = 'Error! Could not reach the API. ' + error
+        })
+    }
+  }
+})
