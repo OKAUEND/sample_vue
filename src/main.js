70bb7aae36a79ee45c91c7103405ec34a6b886ab
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-07-11 19:42:50 +0900

    公式チュートリアル:終了

--- src/main.js
+++ src/main.js
@@ -481,8 +481,8 @@ new Vue({
 })
 
 Vue.component('blog-post',{
-  props:['postTitle'],
-  props: {
+  props:
+  {
     title: String,
     likes: Number,
     isPublished: Boolean,
@@ -502,5 +502,52 @@ new Vue({
       title: 'My Journey with Vue'
     }
   },
+})
 
+Vue.component('base-checkbox',{
+  model:{
+    prop:'checked',
+    event:'change'
+  },
+  props:{
+    checked:Boolean
+  },
+  template: `
+    <input
+      type="checkbox"
+      v-bind:checked="checked"
+      v-on:change="$emit('change', $event.target.checked)"
+    >
+  `
+})
+
+new Vue({
+  el:"#base-check"
+})
+
+Vue.component('base-input',{
+  inheritAttrs:false,
+  props:['label','value'],
+  computed: {
+    inputListeners: function () {
+      var vm = this
+      return Object.assign({},
+        this.$listeners,
+        {
+          input:function(event){
+            vm.$emit('input',event.target.value)
+          }
+        }
+      )
+    }
+  },
+  template:
+  `<label>
+    {{label}}
+    <input
+      v-bind=$attrs
+      v-bind:value="value"
+      v-on="inputListeners
+    >
+    </input>`
 })
