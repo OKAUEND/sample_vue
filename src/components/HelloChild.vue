341a55e7fac1883d68d46ec374ca2dd549bf398e
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-07-12 18:24:14 +0900

    新しい学習内容の準備

---
+++ src/components/HelloChild.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>{{title}}</p>
+        <!-- ここにpropsに設定された値を表示留守 -->
+        <p>{{message}}</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data:function(){
+        return{
+            title:"Hello Child!"
+        }
+    },
+    props:['message']
+}
+</script>
