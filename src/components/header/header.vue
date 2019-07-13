b87bf3cd0d4ac34c8a64b0461031240671aed5c3
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-07-14 01:36:13 +0900

    自作でコンポーネントを作成

---
+++ src/components/header/header.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="nav">
+        <h2>{{title}}</h2>
+        <p class="hage" v-bind="Test">ハゲ</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"header",
+  data:function(){
+  return{
+      title:'Hello Header!!'
+    }
+  }
+}
+</script>
+
+<style>
+.hage
+{
+  color:brown
+}
+
+.nav
+{
+  display:flex;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Header from './components/header/header.vue'
 
 Vue.config.productionTip = false
 
@@ -7,4 +8,10 @@ new Vue({
   el:"#app",
   template:'<App/>',
   components:{App}
+})
+
+new Vue({
+  el:"#header",
+  template:'<Header/>',
+  components:{Header}
 })
