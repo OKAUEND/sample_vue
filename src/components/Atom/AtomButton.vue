939213b23709078a46e64c569d976e444867e904
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-07-25 08:39:22 +0900

    ButtonComponentのイベント名、CSSをローカル化対応

--- src/components/Atom/AtomButton.vue
+++ src/components/Atom/AtomButton.vue
@@ -2,7 +2,7 @@
         <button 
             class="BaseButton"
             v-bind:class="ClassName" 
-            v-on="onClick"
+            v-on:click="onClick"
         >
         <slot name="ButtonText"></slot>
         </button>
@@ -17,31 +17,39 @@ export default {
             default:'defaultButton'
         }
     },
-    onClick:function()
-    {
-        return this.$emit('click');
+    methods:{
+        onClick:function()
+        {
+            return this.$emit('onClick');
+        }
     }
+
 }
 </script>
 
-<style>
+<style scoped>
 .BaseButton
 {
     margin:0;
     padding:0;
     width:100%;
+    height: 100%;
     background-color:#2c2c2c;
 }
 
 .defaultButton
 {
-
+    margin:0;
 }
 
 .SearchButton
 {
-    background-color:#2c2c2c;
-    border: 1.5px solid #cfcfcf;
+    border-top: 1.5px solid #cfcfcf;
+    border-right: 1.5px solid  #cfcfcf;
+    border-bottom: 1.5px solid  #cfcfcf;
+    border-left: 0px;
+    border-radius: 0px 5px 5px 0px;
     color:#cfcfcf;
+    
 }
 </style>
