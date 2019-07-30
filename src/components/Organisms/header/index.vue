b69fdfa946ed7b1eea1604959df0e0bdb7126437
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-07-30 09:30:05 +0900

    ヘッダー部分をOrganismsとしてファイル場所を移動するのと、フォルダを新規作成

---
+++ src/components/Organisms/header/index.vue
@@ -0,0 +1,64 @@
+<template>
+      <nav id="nav">
+        <div class="sideitem">
+          <h2>{{title}}</h2>
+        </div>
+        <div class="headerlogo">
+
+        </div>
+        <div class="sideitem">
+          <search-form>
+          </search-form>
+        </div>
+      </nav>
+</template>
+
+<script>
+import SearchForm from "@/components/Molecules/SearchForm/index.vue"
+export default {
+    name:"test-header",
+    data:function(){
+    return{
+        title:'Hello Header!!',
+        img_src:'@/asset/logo.png'
+      }
+    },
+    components:{
+      SearchForm,
+    }
+}
+</script>
+
+<style>
+#nav
+{
+  display:flex;
+  background-color: #2e2e2e;
+  align-items: center;
+  right: 0;
+  left: 0;
+  height: 70px;
+}
+
+h2
+{
+  margin:0;
+  padding: 0;
+}
+
+#nav > div
+{
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+
+#test
+{
+  width: 400px;
+  height: 50px;
+  background-color: azure;
+}
+
+</style>
