35ace7365d5ebd7cece2d22e2a1b741669df4ca3
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-07-30 09:30:51 +0900

    検索フォームの試しコーディングが一定以上の成果を得たので、コードを削除

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,16 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld/>
-    <SearchForm/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import SearchForm from './components/Molecules/MoleculesSearchForm.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
-    SearchForm,
   }
 }
 </script>
