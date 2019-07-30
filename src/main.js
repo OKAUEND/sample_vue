ed6dbc937eece10cc3db33f414ac920f7baf04d3
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-07-30 09:30:15 +0900

    同上

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Header from './components/header/header.vue'
+import Header from '@/components/Organisms/header/index.vue'
 
 Vue.config.productionTip = false
 
