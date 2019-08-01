bf596a9afb9eb152ae75240f4168a26c3e4d4653
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-08-02 04:36:43 +0900

    ImageのAtomComponentを作成 CSSは現在仮適応中

---
+++ src/components/Atom/AtomImage.vue
@@ -0,0 +1,51 @@
+<template>
+    <img 
+        class="Test_radius"
+        v-bind:src="path">
+</template>
+
+<script>
+export default {
+    name:"atom-image",
+    props:{
+        path:{
+            type:String,
+            default:""
+        },
+        ClassName:{
+            type:String,
+            default:"img_default"
+        }
+    },
+}
+</script>
+
+<style scoped>
+/* .img_default
+{
+
+} */
+.Test_radius
+{
+    border-radius: 50%;
+    height: 50px;
+    width: 50px;
+}
+
+/* Size */
+/* .img--small
+{
+
+}
+
+.img--medium
+{
+
+}
+
+.img--large
+{
+
+} */
+</style>
+
