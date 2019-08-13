2bbb7024343190fb089d2f59403bf504b8b37140
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-08-13 21:12:42 +0900

    v-forをお試しで使ってみた

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,40 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld/>
+    <head-text Text="Test" Level="2">
+    </head-text>
+    <div 
+      v-for="TestItem of TestData"
+      :key="TestItem.id">
+      <item-show
+        v-bind:ImagePath="TestItem.path"
+        v-bind:ItemName="TestItem.name">
+      </item-show>
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import HeadText from '@/components/Atom/AtomHeadText.vue'
+import ItemShow from '@/components/Molecules/SimpleItem/index.vue'
 
 export default {
   name: 'app',
   components: {
     HelloWorld,
+    HeadText,
+    ItemShow
+  },
+  data:function(){
+    return{
+      TestData:[
+        {id:1,path:"icon/logo.png",name:"Test1"},
+        {id:2,path:"icon/logo.png",name:"Test2"},
+        {id:3,path:"icon/logo.png",name:"Test3"},
+        {id:4,path:"icon/logo.png",name:"Test4"},
+      ]
+    }
   }
 }
 </script>
