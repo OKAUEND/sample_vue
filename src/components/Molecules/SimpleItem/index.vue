97129f6af4a6c52a3d71f24dd6251953b979e844
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-08-13 21:09:52 +0900

    画像とテキストがセットとなったコンポーネントを作成 用途はアイテムをリスト表示するときにしようする

---
+++ src/components/Molecules/SimpleItem/index.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="SimpleShow">
+        <item-icon
+            v-bind:img_path="ImagePath">
+        </item-icon>
+        <item-text
+            v-bind:Text="ItemName">
+        </item-text>
+    </div>
+</template>
+
+<script>
+import ItemIcon from '@/components/Atom/AtomImage.vue'
+import ItemText from '@/components/Atom/AtomHeadText.vue'
+export default {
+    name:"mole-simpleItem",
+    components:{
+        ItemIcon,
+        ItemText
+    },
+    props:{
+        ImagePath:{
+            type:String,
+            default:""
+        },
+        ItemName:{
+            type:String,
+            default:""
+        }
+    }
+}
+</script>
+
+<style scoped>
+.SimpleShow{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+</style>
+
