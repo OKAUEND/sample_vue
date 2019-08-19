0448aed205abb525c531f1e9d150a26e14825f98
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-08-19 13:54:20 +0900

    Atomコンポーネントに、SPANを新規作成

---
+++ src/components/Atom/AtomSpan.vue
@@ -0,0 +1,41 @@
+<template>
+    <span 
+        v-bind:class="ClassName">
+        {{ Text }}
+    </span>
+</template>
+
+<script>
+export default {
+    name:"atom-span",
+    props:{
+        Text:{
+            type:String,
+            default:""
+        },
+        ClassName:{
+            type:String,
+            default:""
+        }
+    }
+}
+</script>
+
+<style scoped>
+span{
+    display: block;
+    height: 100%;
+}
+/* Size */
+.--small{
+    font-size: 13px;
+}
+
+.--medium{
+    font-size: 15px;
+}
+
+.--large{
+    font-size: 20px;
+}
+</style>
