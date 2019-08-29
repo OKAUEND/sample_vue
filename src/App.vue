be7e1a854faaad13e022e410143fcc3ed50e56fc
OKAUEND <38197580+OKAUEND@users.noreply.github.com>
2019-08-30 01:26:25 +0900

    v-forとv-modelの機能をいじりながら試す 配列のオブジェクトをv-forで回しながらv-modelで渡す事はできなかった インデックスで指定し、子コンポーネントへ渡す 別の方法としては、v-bindで渡す方法もあり

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,25 @@
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld/>
-    <head-text Text="Test" Level="2">
-    </head-text>
-    <show-content 
-      v-model="ItemData">
-    </show-content>
-    {{ ItemData.ProductionCount }}
+    <head-text Text="Test" Level="2" />
+    <div v-for="(item,index) in DataList"
+        v-bind:key="item.ID">
+      <show-content 
+        v-model="DataList[index]">
+      </show-content> 
+    </div>
+    <!-- {{ this.DataList[0] }} -->
+    <check-box v-bind:test="TESTDATA" v-on:change="onChange">
+    </check-box>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-import HeadText from '@/components/Atom/AtomHeadText.vue'
+import HeadText from '@/components/Atom//HeadText/AtomHeadText.vue'
 import ShowContent from '@/components//Molecules/ProductionContent/index.vue'
+import CheckBox from '@/components/Atom//Input/AtomCheckBox.vue'
+import atomlabel from '@/components/Atom//Text/AtomLabel.vue'
 import 'normalize.css'
 
 export default {
@@ -22,51 +28,118 @@ export default {
   components: {
     HelloWorld,
     HeadText,
-    ShowContent
+    ShowContent,
+    CheckBox,
+    atomlabel
   },
   data:function(){
     return{
-        ItemData:{
-          ID:1,
-          Name:"TEST",
-          ItemLevel:1,
-          ImagePath:"icon/logo.png",
-          ProductionCount:1,
-          ChildItem:[
-            {
-                id:1,
-                childimage:"icon/logo.png",
-                childname:"1"
-            },
-            {
-                id:2,
-                childimage:"icon/logo.png",
-                childname:"2"
-            },
-            {
-                id:3,
-                childimage:"icon/logo.png",
-                childname:"3"
-            },
-                            {
-                id:4,
-                childimage:"icon/logo.png",
-                childname:"4"
-            },
-                            {
-                id:5,
-                childimage:"icon/logo.png",
-                childname:"5"
-            },
-                            {
-                id:6,
-                childimage:"icon/logo.png",
-                childname:"6"
-            },
-          ]
-      }
+        DataList:[
+          {
+            ID:1,
+            Name:"TEST",
+            ItemLevel:1,
+            ImagePath:"icon/logo.png",
+            ProductionCount:1,
+            ChildItem:[
+                {
+                    id:1,
+                    childimage:"icon/logo.png",
+                    childname:"1"
+                },
+                {
+                    id:2,
+                    childimage:"icon/logo.png",
+                    childname:"2"
+                },
+                {
+                    id:3,
+                    childimage:"icon/logo.png",
+                    childname:"3"
+                },
+                                {
+                    id:4,
+                    childimage:"icon/logo.png",
+                    childname:"4"
+                },
+                                {
+                    id:5,
+                    childimage:"icon/logo.png",
+                    childname:"5"
+                },
+                                {
+                    id:6,
+                    childimage:"icon/logo.png",
+                    childname:"6"
+                },
+              ]
+          },
+          {
+                ID:2,
+                Name:"Apple",
+                ItemLevel:2,
+                ImagePath:"icon/logo.png",
+                ProductionCount:1,
+                ChildItem:[
+                    {
+                        id:1,
+                        childimage:"icon/logo.png",
+                        childname:"1"
+                    },
+                    {
+                        id:2,
+                        childimage:"icon/logo.png",
+                        childname:"2"
+                    },
+                    {
+                        id:3,
+                        childimage:"icon/logo.png",
+                        childname:"3"
+                    },
+                                    {
+                        id:4,
+                        childimage:"icon/logo.png",
+                        childname:"4"
+                    },
+                                    {
+                        id:5,
+                        childimage:"icon/logo.png",
+                        childname:"5"
+                    },
+                                    {
+                        id:6,
+                        childimage:"icon/logo.png",
+                        childname:"6"
+                    },
+                  ]
+            }
+    ],
+    TESTDATA:{
+      ID:1,
+      Name:"TEST",
+      ItemLevel:1,
+      ImagePath:"icon/logo.png",
+      ProductionCount:1,
+    },
+    CheckedItem:[],
+  }
+},
+methods:{
+  onChange:function(value){
+    console.log(value)
+    if(this.CheckedItem.length){
+      this.CheckedItem.push(value)
+    }
+
+    const exists = this.CheckedItem.some(List => List.ID === value.ID)
+    if(exists){
+      
+    }
+    else{
+      this.CheckedItem.push(value)
     }
   }
+}
 }
 </script>
 
